11fde5b80626f639d7d75db093315f0f76cc685b
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-09-11 19:42:27 +0600

    services section is done

---
+++ src/Pages/Home/Faces/Face.jsx
@@ -0,0 +1,11 @@
+
+
+const Face = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Face;
--- src/Pages/Home/Services/ServiceCard.jsx
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -1,17 +1,20 @@
 
-const ServiceCard = ({colSpan=1, number, icon, title, subtitle}) => {
+const ServiceCard = ({ number, icon, title, subtitle, iconBGColor, position, border,lastNumber}) => {
     return (
-        <div className={`col-span-${colSpan} relative`}>
+        <div className={`col-span-4 lg:col-span-1 relative ml-8 ${position}`}>
 
-            <p>{number}</p>
-            <div>
+            <h1 className="text-center text-[56px] font-semibold font-lexend text-[#2b3142]">{number}</h1>
+            <h1 className="text-center text-[56px] font-semibold font-lexend relative top-[77px] text-[#2b3142]">{lastNumber}</h1>
+            <p className={`${iconBGColor} p-4 rounded-full w-[65px] z-30 relative`}>
                 {icon}
-            </div>
-            <div/>
-            <h1>{title}</h1>
-            <p>{subtitle}</p>
+            </p>
+            {
+                border && <div className="border-dashed border-t-2 border-r-2 rounded-tr-lg absolute top-[120px] left-[40px] z-10 border-[#647296] w-[316px] h-[66px]"/>
+            }
+            <h1 className="font-lexend mt-6 text-[32px] font-semibold text-white mb-3">{title}</h1>
+            <p className="font-medium">{subtitle}</p>
 
-            
+
         </div>
     );
 };
